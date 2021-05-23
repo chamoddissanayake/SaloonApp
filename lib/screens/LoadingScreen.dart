badc1fa3a4e8fddabce7d8cf80cd369462c5621f
chamoddissanayake <dissanayakechamod@gmail.com>
2021-05-23 22:54:37 +0530

    loading screen done

---
+++ lib/screens/LoadingScreen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Image.asset(
+                  'assets/images/logo/logo.png',
+                  fit: BoxFit.fill,
+                  width: 315,
+                  height: 168,
+                ),
+
+                Image.asset(
+                  "assets/images/loading/spinner-200px.gif",
+                  height: 75.0,
+                  width: 75.0,
+                ),
+
+                // Image.asset(
+                //   "assets/images/loading/scissor.gif",
+                //   height: 75.0,
+                //   width: 75.0,
+                // )
+                // DotLoader()
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
