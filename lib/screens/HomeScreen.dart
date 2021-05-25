97893d44316b6c3369273b55a2c956e33591b2d8
chamoddissanayake <dissanayakechamod@gmail.com>
2021-05-25 18:36:30 +0530

    carousel modified

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,7 +1,6 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:saloon_app/screens/TrendingStylesScreen.dart';
 import 'package:saloon_app/widgets/AppBarWidget.dart';
 import 'package:saloon_app/widgets/MainDrawer.dart';
 
@@ -14,12 +13,13 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final List<String> imagelist = [
-    "https://i.imgur.com/zuG2bGQ.jpg",
-    "https://i.imgur.com/zuG2bGQ.jpg",
-    "https://i.imgur.com/zuG2bGQ.jpg",
-    "https://i.imgur.com/zuG2bGQ.jpg",
-    "https://i.imgur.com/zuG2bGQ.jpg"
+    "https://drive.google.com/uc?export=view&id=1dqCbX6fW7LeSi6YTdlpaQMkxow1QqurG",
+    "https://drive.google.com/uc?export=view&id=1mRh8DolV1V70NCCW-OxLfVYZ6yuxiVkg",
+    "https://drive.google.com/uc?export=view&id=1kzmMBrGVGeYa7Fb6SDPcWrmfhpvIcvOf",
+    "https://drive.google.com/uc?export=view&id=1gQe7TXOiS3SHHb4Wq_m2jW8ZZ48VGp0Z",
+    "https://drive.google.com/uc?export=view&id=1cZp4vgoVJCvvgcgc0H5bOmHZKHW47KUr"
   ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,23 +31,28 @@ class _HomeScreenState extends State<HomeScreen> {
         child: SingleChildScrollView(child: MainDrawer()),
       ),
 
-      body: Center(
-        child: CarouselSlider(
-          options: CarouselOptions(
-              enlargeCenterPage: true,
-              enableInfiniteScroll: false,
-              autoPlay: true
-          ),
-          items: imagelist.map((e) => ClipRRect(
-            borderRadius: BorderRadius.circular(8),
-            child: Stack(
-              fit: StackFit.expand,
-              children: <Widget>[
-                Image.network(e,width: 1000,height: 200,fit: BoxFit.cover,)
-              ],
+      body: Container(
+        child: Column(
+          children: [
+            Text(""),
+            CarouselSlider(
+              options: CarouselOptions(
+                  enlargeCenterPage: true,
+                  enableInfiniteScroll: false,
+                  autoPlay: true
+              ),
+              items: imagelist.map((e) => ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: Stack(
+                  fit: StackFit.expand,
+                  children: <Widget>[
+                    Image.network(e,width: 1000,height: 200,fit: BoxFit.cover,)
+                  ],
+                ),
+              )).toList(),
             ),
-          )).toList(),
-        )
+          ],
+        ),
       ),
     );
   }
