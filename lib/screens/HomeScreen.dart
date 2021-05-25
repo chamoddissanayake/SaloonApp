139f6e5a5064da2b778a7796c4dd159155658e55
chamoddissanayake <dissanayakechamod@gmail.com>
2021-05-25 12:47:22 +0530

    app bar modified

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -18,47 +18,10 @@ class _HomeScreenState extends State<HomeScreen> {
       //   title: Text('Home screen Title'),
       // ),
       // appBar: setAppBar(),
-      appBar: AppBar(
-          //backgroundColor: Colors.blue,
-          //automaticallyImplyLeading: true
-          elevation: 0.0,
-          // for elevation
-          titleSpacing: 0.0,
-          // if you want remove title spacing with back button
-          title: Text('Saloon ABC'),
-          // UtilCommonWidget.addTextMedium('About US', Colors.white, 20.0, 1),
-          actions: <Widget>[
-            // addAppBarActionWidgetProfile(icon, 30.0, 30.0, 15.0) // add your custom action widget
-            // Icon(CupertinoIcons.bars)
-            GestureDetector(
-                onTap: () {
-                  print("Search pressed");
-                },
-                child: Container(
-                  child: Icon(CupertinoIcons.search),
-                  // color: Colors.yellow,
-                ))
-          ],
-          //Action icon search as search icon, notification icon
-          leading: new Material(
-              //Custom leading icon, such as back icon or other icon
-              color: Colors.transparent,
-              child: new InkWell(
-                onTap: () {
-                  Navigator.of(context).pop();
-                },
-                // splashColor: Colors.amber,
-                child: GestureDetector(
-                  onTap: (){
-                    print("bars pressed");
-                  },
-                  child: new Container(
-                      // color: Colors.red,
-                      padding: const EdgeInsets.fromLTRB(12.0, 16.0, 16.0, 16.0),
-                      // child: UtilCommonWidget.addImage(Constant.iconBack, 19.0, 10.0))
-                      child: Icon(CupertinoIcons.bars)),
-                ),
-              ))),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(50.0),
+        child:AppBarWidget() ,
+      ),
 
       body: Center(
         child: ElevatedButton(
@@ -75,34 +38,59 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
+}
 
-//
-// Widget setAppBar() {
-//   return new AppBar(
-//     //backgroundColor: Colors.blue,
-//     //automaticallyImplyLeading: true
-//       elevation: 0.0, // for elevation
-//       titleSpacing: 0.0, // if you want remove title spacing with back button
-//       title: Text('Title'),// UtilCommonWidget.addTextMedium('About US', Colors.white, 20.0, 1),
-//       actions: <Widget>[
-//         // addAppBarActionWidgetProfile(icon, 30.0, 30.0, 15.0) // add your custom action widget
-//         Icon(CupertinoIcons.add_circled_solid)
-//       ],//Action icon search as search icon, notification icon
-//       leading: new Material( //Custom leading icon, such as back icon or other icon
-//         color: Colors.transparent,
-//         child: new InkWell(
-//             onTap: () {
-//               Navigator.of(context).pop();
-//             },
-//             splashColor: Colors.amber,
-//             child: new Container(
-//                 padding: const EdgeInsets.fromLTRB(12.0, 16.0, 16.0, 16.0),
-//                 // child: UtilCommonWidget.addImage(Constant.iconBack, 19.0, 10.0))
-//                 child:  Icon(CupertinoIcons.add_circled_solid),
-//         ),
-//       )
-//   )
-//   );
+class AppBarWidget extends StatelessWidget {
+  const AppBarWidget({
+    Key key,
+  }) : super(key: key);
 
-// }
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+        //backgroundColor: Colors.blue,
+        //automaticallyImplyLeading: true
+        elevation: 0.0,
+        // for elevation
+        titleSpacing: 0.0,
+        // if you want remove title spacing with back button
+        title: Text('Saloon ABCD'),
+        // UtilCommonWidget.addTextMedium('About US', Colors.white, 20.0, 1),
+        actions: <Widget>[
+          // addAppBarActionWidgetProfile(icon, 30.0, 30.0, 15.0) // add your custom action widget
+          // Icon(CupertinoIcons.bars)
+          GestureDetector(
+              onTap: () {
+                print("Search pressed");
+              },
+              child: Container(
+                margin: new EdgeInsets.symmetric(horizontal: 15.0),
+                child: Icon(CupertinoIcons.search),
+                // color: Colors.yellow,
+              ))
+        ],
+        //Action icon search as search icon, notification icon
+        leading: new Material(
+            //Custom leading icon, such as back icon or other icon
+            color: Colors.transparent,
+            child: new InkWell(
+              onTap: () {
+                Navigator.of(context).pop();
+              },
+              // splashColor: Colors.amber,
+              child: GestureDetector(
+                onTap: () {
+                  print("bars pressed");
+                },
+                child: new Container(
+                    // color: Colors.red,
+                    padding:
+                        const EdgeInsets.fromLTRB(12.0, 16.0, 16.0, 16.0),
+                    // child: UtilCommonWidget.addImage(Constant.iconBack, 19.0, 10.0))
+                    child: Icon(CupertinoIcons.bars)),
+              ),
+            )
+        )
+    );
+  }
 }
